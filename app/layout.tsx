2d2c6ef6439974966af97f47a502df20f0235969
Subidh Khanal <subidhkhanal37@gmail.com>
2024-09-04 17:07:55 +0545

    added google analytics too

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,16 +20,33 @@ export default function RootLayout({
     <html lang="en">
       <head>
         {process.env.NODE_ENV === "production" && (
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `(function(c,l,a,r,i,t,y){
+          <>
+            <script
+              type="text/javascript"
+              dangerouslySetInnerHTML={{
+                __html: `(function(c,l,a,r,i,t,y){
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})
               (window, document, "clarity", "script", "nxx12m21d8");`,
-            }}
-          />
+              }}
+            />
+            {/* Google Analytics Global Site Tag (gtag.js) */}
+            <script
+              async
+              src="https://www.googletagmanager.com/gtag/js?id=G-57T2PMMFKG"
+            />
+            <script
+              dangerouslySetInnerHTML={{
+                __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-57T2PMMFKG');
+                `,
+              }}
+            />
+          </>
         )}
       </head>
       <body className={inter.className}>{children}</body>

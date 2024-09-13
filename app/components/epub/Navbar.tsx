4aff3b13fdb90b6d9e6a8656e58e9c6ef9e620d3
Subidh Khanal <subidhkhanal37@gmail.com>
2024-09-13 13:38:21 +0545

    added home arrow and full screen

--- app/components/epub/Navbar.tsx
+++ app/components/epub/Navbar.tsx
@@ -1,7 +1,9 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineToc } from "react-icons/md";
+import { FaExpand, FaCompress, FaArrowLeft } from "react-icons/fa";
+import Link from "next/link";
 
 interface NavbarProps {
   title: string; // The title of the book
@@ -18,44 +20,82 @@ const Navbar: React.FC<NavbarProps> = ({
   setIsNavbarActive,
   setIsTOCVisible,
 }) => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const handleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+      setIsFullscreen(true);
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+    }
+  };
   return (
     <nav
-      className={`p-4 flex items-center justify-between shadow-md transition-all duration-300 ${
+      className={`pl-1.5	p-4 flex items-center justify-between shadow-md transition-all duration-300 ${
         isDarkTheme
           ? "bg-gradient-to-r from-[#2c2c38] to-[#3a3a4a]"
           : "bg-[#d3d3e0]"
       } rounded-lg`}
       onClick={() => setIsNavbarActive(true)}
     >
-      <span
-        className={`text-lg font-semibold ${isDarkTheme ? " " : "text-[#333]"}`}
-      >
-        {title}
-      </span>
-      <button
-        onClick={() => {
-          setIsTOCVisible(!isTOCVisible); // Invert the current navbar visibility
-        }}
-        aria-label={
-          isTOCVisible ? "Hide Table of Contents" : "Show Table of Contents"
-        }
-        className={`p-2 text-sm rounded-md transition-transform hover:scale-105 active:scale-95 ${
-          isTOCVisible
-            ? isDarkTheme
-              ? "bg-[#5a5a68] text-gray-100 shadow-lg"
-              : "bg-[#d3d3e0] text-gray-800 shadow-lg"
-            : isDarkTheme
-            ? "bg-transparent text-gray-100 hover:bg-[#444455] hover:text-white"
-            : "bg-transparent text-gray-800 hover:bg-[#dde1f9]  hover:text-black hover:text-gray-900"
-        }`}
-        style={{ transition: "background-color 0.3s ease, color 0.3s ease" }}
-      >
-        {/* <FiMenu size={24} /> */}
-        <MdOutlineToc
-          style={{ transform: "scaleX(-1) scale(1.3)" }}
-          size={24}
-        />
-      </button>
+      <div className="flex items-center">
+        <div
+          className={`mr-3.5 p-2  flex transform scale-110 p-2 rounded-full cursor-pointer transition-transform duration-300 ${
+            isFullscreen ? "" : " "
+          } ${isDarkTheme ? "hover:bg-[#444455]" : "hover:bg-[#dde1f9]"}
+        `}
+        >
+          <Link href="/">
+            <FaArrowLeft size={17} />
+          </Link>
+        </div>
+
+        <div
+          className={`text-lg font-semibold ${
+            isDarkTheme ? " " : "text-[#333]"
+          }`}
+        >
+          {title}
+        </div>
+      </div>
+      <div className="flex items-center">
+        <button
+          onClick={() => {
+            setIsTOCVisible(!isTOCVisible); // Invert the current navbar visibility
+          }}
+          aria-label={
+            isTOCVisible ? "Hide Table of Contents" : "Show Table of Contents"
+          }
+          className={`p-2 text-sm rounded-md transition-transform hover:scale-105 active:scale-95 mr-[21px] ${
+            isTOCVisible
+              ? isDarkTheme
+                ? "bg-[#5a5a68] text-gray-100 shadow-lg"
+                : "bg-[#d3d3e0] text-gray-800 shadow-lg"
+              : isDarkTheme
+              ? "bg-transparent text-gray-100 hover:bg-[#444455] hover:text-white"
+              : "bg-transparent text-gray-800 hover:bg-[#dde1f9]  hover:text-black hover:text-gray-900"
+          }`}
+          style={{ transition: "background-color 0.3s ease, color 0.3s ease" }}
+        >
+          {/* <FiMenu size={24} /> */}
+          <MdOutlineToc
+            style={{ transform: "scaleX(-1) scale(1.3)" }}
+            size={24}
+            className="scale-125"
+          />
+        </button>
+        <div
+          onClick={handleFullscreen}
+          className={`transform scale-110 p-2 rounded-full cursor-pointer transition-transform duration-300 ${
+            isFullscreen ? "" : " "
+          } ${isDarkTheme ? "hover:bg-[#444455]" : "hover:bg-[#dde1f9]"}
+        `}
+        >
+          {isFullscreen ? <FaCompress size={20} /> : <FaExpand size={20} />}
+        </div>
+      </div>
     </nav>
   );
 };

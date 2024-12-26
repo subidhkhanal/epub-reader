01384d645d4ad9c201b06f20f622f4ea54e0c7b4
Subidh Khanal <subidhkhanal37@gmail.com>
2024-12-26 14:15:22 +0530

    making changes to the landing page navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -97,118 +97,97 @@ const NavBar: React.FC<NavBarProps> = ({
 
   return (
     <header
-      className={`fixed top-0 left-0 w-full z-1000 flex justify-between items-center p-4 pl-2 transition-all duration-300 ${
+      className={`fixed top-0 left-0 w-full z-[1000] flex justify-between items-center p-4 pl-2 transition-all duration-300 ${
         isDarkTheme
-          ? "bg-[#18212f] text-[#F3F4F6] shadow-lg" // Dark theme styles with slate gray background
-          : "bg-gradient-to-br from-[#1d2b64] to-[#f8cdda] text-white shadow-md" // Light theme styles
+          ? "bg-[#18212f] text-[#F3F4F6] shadow-lg"
+          : "bg-gradient-to-br from-[#1d2b64] to-[#f8cdda] text-white shadow-md"
       }`}
     >
-      {user ? (
-        <>
-          {/* Left: Menu and Logo */}
-          <div className="flex items-center">
-            <FaBars
-              className="text-lg cursor-pointer [@media(min-width:786px)]:hidden block"
-              onClick={onMenuClick}
-            />
-            <Link href="/">
-              <span className="ml-2 font-bold text-xl [@media(min-width:786px)]:block hidden">
-                My Books
-              </span>
-            </Link>
-          </div>
+      {/* Left: Menu and Logo */}
+      <div className="flex items-center">
+        <FaBars
+          className="text-lg cursor-pointer md:hidden block"
+          onClick={onMenuClick}
+        />
+        <Link href="/">
+          <span className="ml-2 font-bold text-xl hidden md:block">
+            My Books
+          </span>
+        </Link>
+      </div>
 
-          {/* Middle: Search Bar */}
-          <div className="flex-1 [@media(min-width:786px)]:flex justify-center items-center">
-            {showSmallSearch ? (
-              <div
-                className={`[@media(min-width:786px)]:hidden flex items-center rounded-full px-4 py-2 w-full max-w-xl ${
-                  isDarkTheme
-                    ? "bg-[#111827] text-[#D1D5DB] shadow-inner" // Dark theme styles with dark navy background
-                    : "bg-white text-gray-800 shadow-md" // Light theme styles
-                }`}
-                ref={smallSearchRef}
-              >
-                <input
-                  type="text"
-                  placeholder="Search"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)} // Update searchTerm
-                  className="w-full bg-transparent border-none outline-none text-base"
-                />
-              </div>
-            ) : (
-              <div
-                className={`[@media(min-width:786px)]:flex hidden items-center rounded-full px-4 py-2 w-full max-w-xl ${
-                  isDarkTheme
-                    ? "bg-[#111827] text-[#D1D5DB] shadow-inner" // Dark theme styles with dark navy background
-                    : "bg-white text-gray-800 shadow-md" // Light theme styles
-                }`}
-              >
-                <FaSearch className="text-gray-400 mr-2 [@media(min-width:786px)]:block hidden" />
-                <input
-                  type="text"
-                  placeholder="Search"
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)}
-                  className="flex-grow bg-transparent border-none outline-none text-base [@media(min-width:786px)]:block hidden"
-                />
-              </div>
-            )}
+      {/* Middle: Search Bar */}
+
+      {user ? (
+        <div className="flex-1 flex justify-center items-center">
+          <div
+            className={`flex items-center rounded-full px-4 py-2 w-full max-w-xl ${
+              isDarkTheme
+                ? "bg-[#111827] text-[#D1D5DB] shadow-inner"
+                : "bg-white text-gray-800 shadow-md"
+            }`}
+          >
+            <FaSearch className="text-gray-400 mr-2 hidden md:block" />
+            <input
+              type="text"
+              placeholder="Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="flex-grow bg-transparent border-none outline-none text-base"
+            />
           </div>
+        </div>
+      ) : (
+        <></>
+      )}
 
       {/* Right: Theme Toggle and User Profile */}
-      <div className="flex items-center relative">
+      <div className="flex items-center">
         {user ? (
-          /* Small Devices: Search Icon and Search Input */
-          <div
-            className="[@media(min-width:786px)]:hidden"
-            ref={smallSearchRef}
-          >
-            <FaSearch
-              className={`text-gray-400 mr-4 cursor-pointer ${
-                showSmallSearch ? "hidden" : "block"
-              }`}
-              onClick={() => {
-                setShowSmallSearch(true); // Show the search input
-              }}
-            />
-          </div>
-        ) : (
-          " "
-        )}
-        <div className="[@media(min-width:786px)]:block hidden">
-          <AddBook isDarkTheme={isDarkTheme} />
-        </div>
-        <ThemeToggleButton />
-
-        {user ? (
-          <div className="flex items-center ml-4">
-            <img
-              src={user.photoURL || "/default-avatar.png"}
-              alt="User Avatar"
-              className="w-10 h-10 rounded-full mr-2 cursor-pointer"
-              onClick={toggleDropdown}
-            />
-            {dropdownVisible && (
-              <div
-                ref={dropdownRef}
-                className={`absolute top-12 right-0 mt-2 w-48 ${
-                  isDarkTheme ? "bg-[#18212f] text-gray-200" : "bg-white"
-                } rounded-lg shadow-lg overflow-hidden z-50`}
-              >
-                <button
-                  onClick={handleSignOut}
-                  className={`block w-full px-4 py-2 text-left ${
-                    isDarkTheme
-                      ? "hover:bg-[#111827] text-white"
-                      : "hover:bg-gray-100 text-gray-800"
-                  }`}
+          <div>
+            {/* /* Mobile Search */}
+            <div className="md:hidden">
+              <FaSearch
+                className={`text-gray-400 mr-4 cursor-pointer ${
+                  showSmallSearch ? "hidden" : "block"
+                }`}
+                onClick={() => setShowSmallSearch(true)}
+              />
+            </div>
+            {/* Add Book (Desktop Only) */}
+            <div className="hidden md:block">
+              <AddBook isDarkTheme={isDarkTheme} />
+            </div>
+            {/* Theme Toggle */}
+            <ThemeToggleButton />
+            {/* User Profile */}
+            <div className="flex items-center ml-4 relative">
+              <img
+                src={user.photoURL || "/default-avatar.png"}
+                alt="User Avatar"
+                className="w-10 h-10 rounded-full mr-2 cursor-pointer"
+                onClick={toggleDropdown}
+              />
+              {dropdownVisible && (
+                <div
+                  ref={dropdownRef}
+                  className={`absolute top-12 right-0 mt-2 w-48 ${
+                    isDarkTheme ? "bg-[#18212f] text-gray-200" : "bg-white"
+                  } rounded-lg shadow-lg overflow-hidden z-50`}
                 >
-                  Log Out
-                </button>
-              </div>
-            )}
+                  <button
+                    onClick={handleSignOut}
+                    className={`block w-full px-4 py-2 text-left ${
+                      isDarkTheme
+                        ? "hover:bg-[#111827] text-white"
+                        : "hover:bg-gray-100 text-gray-800"
+                    }`}
+                  >
+                    Log Out
+                  </button>
+                </div>
+              )}
+            </div>
           </div>
         ) : (
           <button
